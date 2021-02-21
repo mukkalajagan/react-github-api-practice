aa6842cd083543f08ecfda3f101e2dc029bcbd78
mukkalajagan <jaganmohan.mukkala@gmail.com>
2021-02-22 00:25:42 +0530

    2 way binding(not really) in React

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const App = () => {
   const [variable, changeVariable] = useState("");
-  const [vel, setVel] = useState([]);
+
   const handleChange = e => {
     changeVariable(e.target.value);
   };
 
-  useEffect(async () => {
-    console.log(variable);
-    const result = await axios(
-      "https://hn.algolia.com/api/v1/search?query=redux"
-    );
-    setVel(result.data);
-    console.log(`This is result : {{vel}}`);
-  }, [variable]);
-
   return (
     <div>
-      <input type="text" onChange={handleChange} />
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="Enter something"
+      />
       <br />
       <br />
       {variable}
-      <br />
-      <br />
-      {vel}
     </div>
   );
 };
