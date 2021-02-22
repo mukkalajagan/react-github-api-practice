7ba133fcb40017fb160c3a135a669e3d0cd24659
mukkalajagan <jaganmohan.mukkala@gmail.com>
2021-02-22 16:06:16 +0530

    Getting input value by using 'useRef' & 'ref'

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const App = () => {
   const [variable, changeVariable] = useState("");
-
+  let inputRef = useRef("jagan");
   const handleChange = e => {
     changeVariable(e.target.value);
   };
-
+  const handleClick = () => {
+    console.log(inputRef.current.value);
+    changeVariable(inputRef.current.value);
+  };
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [inputRef.current]);
   return (
     <div>
-      <input
-        type="text"
-        onChange={handleChange}
-        placeholder="Enter something"
-      />
+      <input type="text" placeholder="Enter something" ref={inputRef} />
+      <br />
+      <br />
+      <button onClick={handleClick}><img src=''/>Click Me</button>
       <br />
       <br />
       {variable}
